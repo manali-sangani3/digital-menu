bf9aefcfb355ee1d4ff7cd76828ca4dd391ed88b
Manali <manali.sangani@neosoftmail.com>
2026-06-11 09:41:03 +0530

    feat: configure firebase hosting and refactor remote datasource code formatting

--- digital_menu/lib/features/menu/data/datasources/menu_remote_datasource.dart
+++ digital_menu/lib/features/menu/data/datasources/menu_remote_datasource.dart
@@ -13,27 +13,29 @@ class MenuRemoteDataSourceImpl implements MenuRemoteDataSource {
 
   MenuRemoteDataSourceImpl(this._firestore);
 
-  CollectionReference<MenuCategoryModel> get _categoriesRef =>
-      _firestore.collection(FirestoreCollections.categories).withConverter<MenuCategoryModel>(
-            fromFirestore: (snapshot, _) => MenuCategoryModel.fromJson(
-              (snapshot.data() ?? {})..putIfAbsent('id', () => snapshot.id),
-            ),
-            toFirestore: (model, _) => model.toJson()..remove('id'),
-          );
+  CollectionReference<MenuCategoryModel> get _categoriesRef => _firestore
+      .collection(FirestoreCollections.categories)
+      .withConverter<MenuCategoryModel>(
+        fromFirestore: (snapshot, _) => MenuCategoryModel.fromJson(
+          (snapshot.data() ?? {})..putIfAbsent('id', () => snapshot.id),
+        ),
+        toFirestore: (model, _) => model.toJson()..remove('id'),
+      );
 
-  CollectionReference<DishModel> get _dishesRef =>
-      _firestore.collection(FirestoreCollections.dishes).withConverter<DishModel>(
-            fromFirestore: (snapshot, _) => DishModel.fromJson(
-              (snapshot.data() ?? {})..putIfAbsent('id', () => snapshot.id),
-            ),
-            toFirestore: (model, _) => model.toJson()..remove('id'),
-          );
+  CollectionReference<DishModel> get _dishesRef => _firestore
+      .collection(FirestoreCollections.dishes)
+      .withConverter<DishModel>(
+        fromFirestore: (snapshot, _) => DishModel.fromJson(
+          (snapshot.data() ?? {})..putIfAbsent('id', () => snapshot.id),
+        ),
+        toFirestore: (model, _) => model.toJson()..remove('id'),
+      );
 
   @override
   Stream<List<MenuCategoryModel>> streamCategories() {
     return _categoriesRef.snapshots().map(
-          (snapshot) => snapshot.docs.map((doc) => doc.data()).toList(),
-        );
+      (snapshot) => snapshot.docs.map((doc) => doc.data()).toList(),
+    );
   }
 
   @override
@@ -41,8 +43,6 @@ class MenuRemoteDataSourceImpl implements MenuRemoteDataSource {
     return _dishesRef
         .where('categoryId', isEqualTo: categoryId)
         .snapshots()
-        .map(
-          (snapshot) => snapshot.docs.map((doc) => doc.data()).toList(),
-        );
+        .map((snapshot) => snapshot.docs.map((doc) => doc.data()).toList());
   }
 }
